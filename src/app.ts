72c50931f01c7ae2767dfb0d82ca5fcf25f8a9c5
Sergey Shaporov <sshaporov86@gmail.com>
2021-01-16 21:48:50 +0300

    add test data for cards router

--- src/app.ts
+++ src/app.ts
@@ -13,26 +13,26 @@ const corsOptions = {
     //     return callback(null, true)
     //
     // callback(new Error('Not allowed by CORS'));
-    console.log("origin: ", origin);
+    //console.log("origin: ", origin);
     callback(null, true); // everyone is allowed
   }
 };
 
 // подключаемся к базе данных Мангус (оборачиваем в функцию чтобы использовать асинк/авет, а не обрабатывать промис)
-async function start() {
-  try {
-    // функция возвращет промис
-    await mongoose.connect(config.get('mongoUri'), {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    })
-  } catch (error) {
-    console.log('Server error', error)
-    process.exit(1)
-  }
-}
-start()
+// async function start() {
+//   try {
+//     // функция возвращет промис
+//     await mongoose.connect(config.get('mongoUri'), {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useCreateIndex: true,
+//     })
+//   } catch (error) {
+//     console.log('Server error', error)
+//     process.exit(1)
+//   }
+// }
+// start()
 
 const PORT = config.get('port') || 3010
 
@@ -41,9 +41,14 @@ app.get("/", (req, res) => {
   res.send("<h1>1</h1>")
 })
 
+
+const cards = [
+  {id: '1', title: "What to lear"},
+  {id: '2', title: "Travel tasks"},
+]
 app.get("/cards", cors(corsOptions), (req, res) => {
   console.log('work route "/cards"')
-  res.send({cardsId: "test card id"})
+  res.send(cards)
 })
 
 app.listen(PORT, () => {
