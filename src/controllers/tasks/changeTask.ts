7b136b5174ef09994af74de7c6e37b6ea9874cf8
Sergey Shaporov <sshaporov86@gmail.com>
2021-01-21 22:02:33 +0300

    refactoring changeTask for status and title

--- src/controllers/tasks/changeTask.ts
+++ src/controllers/tasks/changeTask.ts
@@ -2,19 +2,11 @@ import {Request, Response} from 'express'
 import Task from '../../models/tasks-model'
 
 export const changeTask = async (req: Request, res: Response) => {
-
-  const keys = Object.keys(req.body)
-
-  if (keys.length === 1) {
-    if (keys[0] === 'checked') {
-        Task.findByIdAndUpdate(req.params.taskId, {checked: req.body.checked}, {new: true})
-          .then((updatedTask) => {res.status(200).send({item: updatedTask})})
-          .catch(() => {res.status(400)})
-    } else if (keys[0] === 'title') {
-        Task.findByIdAndUpdate(req.params.taskId, {title: req.body.title}, {new: true})
-          .then((updatedTask) => {res.status(200).send({item: updatedTask})})
-          .catch(() => {res.status(400)})
-    } else res.status(400)
-
-  } else res.status(400)
+  Task.findByIdAndUpdate(req.params.taskId, req.body, {new: true})
+    .then((updatedTask) => {
+      res.status(200).send({item: updatedTask})
+    })
+    .catch(() => {
+      res.status(400)
+    })
 }
