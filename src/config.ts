d31952de9734f748cc7d804b82c0c7421f9f7bdd
Sergey Shaporov <sshaporov86@gmail.com>
2021-01-23 22:59:07 +0300

    refactoring app structure

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,7 @@
 export const PORT = 3010
 export const MONGO_URI = "mongodb+srv://admin:Passw0rd@cluster0.ahetv.mongodb.net/myTasks?retryWrites=true&w=majority"
+export const MONGOOSE_CONNECT_OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as cors from 'cors'
 import cards from './routers/cards-router'
 import * as bodyParser from 'body-parser'
 import * as mongoose from 'mongoose'
-import {MONGO_URI, PORT} from './config'
+import {MONGO_URI, MONGOOSE_CONNECT_OPTIONS, PORT} from './config'
 
 const app = express()
 
@@ -12,22 +12,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use('/cards', cards)
 
-app.listen(PORT, () => {
-  console.log(`Server is running in http://localhost:${PORT}`)
-})
+mongoose.connect(MONGO_URI, MONGOOSE_CONNECT_OPTIONS)
+  .then(() => app.listen(PORT, () => {
+    console.log(`Server is running in http://localhost:${PORT}`)
+  }))
+  .catch(err => {
+    console.log('Mongo server error', err)
+    //process.exit(1)
+  })
 
-// подключаемся к базе данных Мангус (оборачиваем в функцию чтобы использовать асинк/авет, а не обрабатывать промис)
-async function start() {
-  try {
-    // функция возвращет промис
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    })
-  } catch (e) {
-    console.log('Mongo server error', e)
-    process.exit(1)
-  }
-}
-start()
+
+
