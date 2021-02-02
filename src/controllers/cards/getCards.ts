3acb4a27c59cf1202e544936f85a53a4f3102b1c
Sergey Shaporov <sshaporov86@gmail.com>
2021-02-02 17:14:28 +0300

    search cards by req.query.search

--- src/controllers/cards/getCards.ts
+++ src/controllers/cards/getCards.ts
@@ -4,11 +4,17 @@ import {IUserRequest} from '../../middleware/authMiddleware'
 
 export const getCards = async (req: IUserRequest, res: Response) => {
   const userId = req.user.id
+  const searchCardTitle = req.query.search
 
   Card.find({user_id: userId})
     .exec()
     .then(cards => {
-      res.status(200).json(cards)
+      if(searchCardTitle == undefined || searchCardTitle == '') {
+        res.status(200).json(cards)
+      } else {
+        const cardsSearch = cards.filter(card => card.title ===  searchCardTitle)
+        res.status(200).json(cardsSearch)
+      }
     })
     .catch(err => {
       res.status(500).json(err)
---
+++ src/controllers/cards/searchCards.ts
@@ -0,0 +1,16 @@
+import {Response} from 'express'
+import Card from '../../models/card-model'
+import {IUserRequest} from '../../middleware/authMiddleware'
+
+export const searchCards = async (req: IUserRequest, res: Response) => {
+  const userId = req.user.id
+
+  Card.find({user_id: userId})
+    .exec()
+    .then(cards => {
+      res.status(200).json(cards)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+}
