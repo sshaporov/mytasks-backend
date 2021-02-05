8b9340a9c9a1cd9f6cb6d7cf10cf520458d2bd6e
Sergey Shaporov <sshaporov86@gmail.com>
2021-02-05 23:17:11 +0300

    add certs and go to the https protocol

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,15 @@ import auth from './routers/auth-router'
 import * as bodyParser from 'body-parser'
 import * as mongoose from 'mongoose'
 import {MONGO_URI, MONGOOSE_CONNECT_OPTIONS, PORT} from './config'
+import * as https from 'https'
+import * as fs from 'fs'
+
+const key = fs.readFileSync('./key.pem')
+const cert = fs.readFileSync('./cert.pem')
 
 const app = express()
+
+const server = https.createServer({key: key, cert: cert }, app)
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -15,12 +22,15 @@ app.use('/cards', cards)
 app.use('/auth', auth)
 
 mongoose.connect(MONGO_URI, MONGOOSE_CONNECT_OPTIONS)
-  .then(() => app.listen(PORT, () => {
-    console.log(`Server is running in http://localhost:${PORT}`)
+  .then(() => server.listen(PORT, () => {
+    console.log(`Server is running in https://localhost:${PORT}`)
   }))
   .catch(err => {
     console.log('Mongo server error', err)
   })
 
+process.on("unhandledRejection", (reason, p) => {
+  console.log("MyTasks: ", reason, p)
+});
 
 
